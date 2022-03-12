e15e9d6e0b38bac4013df90444964824365a8152
fcampbell7 <54756263+fcampbell7@users.noreply.github.com>
2022-03-12 15:25:19 +1300

    Commit 12th March

--- fc.js
+++ fc.js
@@ -6,74 +6,65 @@ function isMobile(){
       }
 }
 
-
+function imageAnimation(){
+  micron.getEle(".picture").interaction("tada").duration("1").timing("linear");
+}
 
 //Menu items
 function homeClick(){
   document.getElementById("home").style="display:block";
+  document.getElementById("education").style="display:none";
+  document.getElementById("experience").style="display:none";
+  document.getElementById("projects").style="display:none";
   //fade content in gently - figure out animations
+  //fadeIn(".home");
 }
 
 function educationClick(){
   document.getElementById("home").style="display:none";
+  document.getElementById("education").style="display:block";
+  document.getElementById("experience").style="display:none";
+  document.getElementById("projects").style="display:none";
+  //fadeIn(".education");
 }
 
 function experienceClick(){
   document.getElementById("home").style="display:none";
-  
+  document.getElementById("education").style="display:none";
+  document.getElementById("experience").style="display:block";
+  document.getElementById("projects").style="display:none";
+  //fadeIn(".experience");
 }
 
 function projectsClick(){
   document.getElementById("home").style="display:none";
+  document.getElementById("education").style="display:none";
+  document.getElementById("experience").style="display:none";
+  document.getElementById("projects").style="display:block";
+  //fadeIn(".projects");
   
 }
 
-function cvClick(){
-  document.getElementById("home").style="display:none";
-  
+function projectClick(num){
+  var projectInfoTag = "projectinfo" + num.toString();
+  var projectTag = "#project" + num.toString();
+  micron.getEle(projectTag).interaction("bounce").duration(".3").timing("ease-in-out");
+
+  document.getElementById("projectinfo1").style="display:none";
+  document.getElementById("projectinfo2").style="display:none";
+  document.getElementById("projectinfo3").style="display:none";
+  document.getElementById("projectinfo4").style="display:none";
+  document.getElementById("projectinfo5").style="display:none";
+
+  document.getElementById(projectInfoTag).style="display:block";
+
 }
 
-function randomGradient(){
-  
-  rand = Math.floor(Math.random() * 4);
-
-  if(rand==0){
-    //document.getElementById("body").style="background:linear-gradient(45deg, #f2ff36, #ffc31e,#15ff48, #b1fd37)";
-  }
-
-  if(rand==1){
-    //document.getElementById("body").style="background:linear-gradient(45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)";
-  }
-
-  if(rand==2){
-    //document.getElementById("body").style="background:linear-gradient(45deg, #d036ff, #ff1e1e,#2c15ff,#15b5ff)";
-  }
-
-  if(rand==3){
-    //document.getElementById("body").style="background:linear-gradient(45deg, #20da29, #1eff96,#f549ad,#ff158a)";
-  }
-  
-}
-
-function animateImage(){
-  //add some sort of subtle animation to image on mouse hover
-  img = document.getElementById("portrait");
-
-  const animateit = function (e) {
-    const span = this.querySelector('span');
-    const { offsetX: x, offsetY: y } = e,
-    { offsetWidth: width, offsetHeight: height } = this,
-
-    move = 25,
-    xMove = x / width * (move * 2) - move,
-    yMove = y / height * (move * 2) - move;
-
-    span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-    if (e.type === 'mouseleave') span.style.transform = '';
-};
-
-img.addEventListener('mousemove', animateit);
-img.addEventListener('mouseleave', animateit);
-
+function projectInfoClick(){
+  //This code is garbage, but it works
+  document.getElementById("projectinfo1").style="display:none";
+  document.getElementById("projectinfo2").style="display:none";
+  document.getElementById("projectinfo3").style="display:none";
+  document.getElementById("projectinfo4").style="display:none";
+  document.getElementById("projectinfo5").style="display:none";
 }
